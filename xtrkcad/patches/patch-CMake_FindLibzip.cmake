85cc191489cffb89c515622f2877fde041d0d971
Yuri Honegger <yuri.honegger@icloud.com>
2026-05-25 19:29:22 +0200

    xtrkcad: don't link against linux libraries

---
+++ xtrkcad/patches/patch-CMake_FindLibzip.cmake
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- CMake/FindLibzip.cmake.orig	2026-05-25 17:27:28.576514792 +0000
++++ CMake/FindLibzip.cmake
+@@ -36,7 +36,7 @@ else(WIN32)
+     NAMES zipconf.h
+     HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+ 
+-  if(UNIX AND NOT APPLE)
++  if(LINUX)
+     find_library(LIBZIP_LIBRARY
+       NAMES libzip.a zip
+       PATHS
